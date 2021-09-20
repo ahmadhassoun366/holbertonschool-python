8377226aba353948e3f5a2ba78abf860d518708b
Live session user <ahmadhss366@gmail.com>
2021-09-20 13:38:13 +0000

    Magix

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,103 +1,37 @@
 #!/usr/bin/python3
-class Node:
-    """Represents a node in a singly linked list
-    Attributes:
-        __data (int): data stored inside the node
-        __next_node (Node): next node in the linked list
+"""
+    103-magic_class.py
+    Module that defines a MagicClass and return circle radius
+    Including Methods to calculate the Circle's Area and Circumference
+"""
+import math
+
+
+class MagicClass:
     """
-    def __init__(self, data, next_node=None):
-        """Initializes the node
-        Args:
-            data (int): data stored inside the node
-            next_node (Node): next node in the linked list
-        Returns:
-            None
-        """
-        self.data = data
-        self.next_node = next_node
-    @property
-    def data(self):
-        """getter of __data
-        Returns:
-            data stored inside the node
-        """
-        return self.__data
-    @data.setter
-    def data(self, value):
-        """setter of __data
-        Args:
-            value (int): data stored insite the node
-        Returns:
-            None
-        """
-        if type(value) is not int:
-            raise TypeError("data must be an integer")
-        self.__data = value
-    @property
-    def next_node(self):
-        """getter of __next_node
-        Returns:
-           the next node in the linked list
-        """
-        return self.__next_node
-    @next_node.setter
-    def next_node(self, value):
-        """setter of __next_node
-        Args:
-            value (Node): next node in the linked list
-        Returns:
-            None
-        """
-        if value is not None and type(value) is not Node:
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = value
-    def __str__(self):
-        """String representation of Node instance
-        Returns:
-            Formatted string representing the node
-        """
-        return str(self.__data)
-class SinglyLinkedList:
-    """Represents a single linked list
-    Attributes:
-        __head (Node): head of the linked list
+    Represents a class called MagicClass with a private instance
+    attribute called radius
     """
-    def __init__(self):
-        """Initializes the linked list
-        Returns:
-            None
+
+    def __init__(self, radius=0):
         """
-        self.__head = None
-    def sorted_insert(self, value):
-        """ inserts a new Node instance into the correct sorted position
+        Initialization of the private instance attribute called radius
         Args:
-            value (int): data stored inside the new node
-        Returns:
-            None
+            radius (int, float): radius of the Circle
+        Note:
+            If radius is not an integer and not float, a TypeError exception
+            is raised
+            Otherwise, Successful Initialization
         """
-        new = Node(value)
-        tmp = self.__head
-        if tmp is None or tmp.data >= value:
-            if tmp:
-                new.next_node = tmp
-            self.__head = new
-            return
-        while tmp.next_node is not None:
-            if tmp.next_node.data >= value:
-                break
-            tmp = tmp.next_node
-        new.next_node = tmp.next_node
-        tmp.next_node = new
-    def __str__(self):
-        """String representation of SinglyLinkedList instance
-        Returns:
-            Formatted string representing the linked list
-        """
-        string = ""
-        tmp = self.__head
-        while tmp is not None:
-            string += str(tmp)
-            if tmp.next_node is not None:
-                string += "\n"
-            tmp = tmp.next_node
-        return string
+        self.__radius = 0
+        if not isinstance(radius, int) and not isinstance(radius, float):
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """Function that returns the area of the Circle"""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Function that returns the circumference of the Circle"""
+        return 2 * math.pi * self.__radius
