3ad936ac913a9fd7eb97686f37981b0344e96efe
Live session user <ahmadhss366@gmail.com>
2021-09-20 20:39:32 +0000

    singly

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,37 +1,76 @@
 #!/usr/bin/python3
 """
-    103-magic_class.py
-    Module that defines a MagicClass and return circle radius
-    Including Methods to calculate the Circle's Area and Circumference
+    This module defines linked lists
+    return {}
 """
-import math
 
 
-class MagicClass:
-    """
-    Represents a class called MagicClass with a private instance
-    attribute called radius
-    """
+class Node():
+    """Node class"""
 
-    def __init__(self, radius=0):
-        """
-        Initialization of the private instance attribute called radius
-        Args:
-            radius (int, float): radius of the Circle
-        Note:
-            If radius is not an integer and not float, a TypeError exception
-            is raised
-            Otherwise, Successful Initialization
-        """
-        self.__radius = 0
-        if not isinstance(radius, int) and not isinstance(radius, float):
-            raise TypeError('radius must be a number')
-        self.__radius = radius
+    def __init__(self, data, next_node=None):
+        """Initialize class."""
+        self.data = data
+        self.next_node = next_node
 
-    def area(self):
-        """Function that returns the area of the Circle"""
-        return self.__radius ** 2 * math.pi
+    @property
+    def data(self):
+        """Retrieving data at Node"""
+        return self.__data
 
-    def circumference(self):
-        """Function that returns the circumference of the Circle"""
-        return 2 * math.pi * self.__radius
+    @data.setter
+    def data(self, value):
+        """Setting the value of Node"""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Retrieving the next Node"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Setting the value of the next Node"""
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList():
+    """Singly linked list class"""
+
+    def __init__(self):
+        """Initializing the class"""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Inserting a node based on its sorted value."""
+        new_node = Node(value)
+        if self.__head is None:
+            self.__head = new_node
+            return
+        if value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+            return
+        curr = self.__head
+        while value >= curr.data:
+            prev = curr
+            if curr.next_node:
+                curr = curr.next_node
+            else:
+                curr.next_node = new_node
+                return
+        prev.next_node = new_node
+        new_node.next_node = curr
+
+    def __str__(self):
+        """The string representation of the class"""
+        string = ""
+        curr = self.__head
+        while curr:
+            string += str(curr.data) + "\n"
+            curr = curr.next_node
+        return string[:-1]
