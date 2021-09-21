95354494f92733c96a9ca8fea9e141aa8d59e261
ahmadhassoun366 <72111331+ahmadhassoun366@users.noreply.github.com>
2021-09-21 11:52:32 +0000

    Delete 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-
-    for x in matrix:
-        val = list(map(lambda mul: mul ** 2, x))
-        new_matrix.append(val)
-
-    return(new_matrix)
