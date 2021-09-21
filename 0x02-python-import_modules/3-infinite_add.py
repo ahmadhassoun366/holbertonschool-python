a02a1c8c78866180309d5ebfb214afb3ca2119f3
Live session user <ahmadhss366@gmail.com>
2021-09-21 14:03:34 +0000

    first commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 import sys
-
-
-def main():
-    ar = sys.argv
-    ar = ar[1:]
-    sum = 0
-    for i in ar:
-        sum += int(i)
-    print('{}'.format(sum))
+sum = 0
 if __name__ == '__main__':
-    main(
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+print(sum)
